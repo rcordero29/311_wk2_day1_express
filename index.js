2aa03eab1a41791a17ad9469f363bb25acd8ac9e
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-12-01 21:14:37 -0600

    another final

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 8000;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-
-const { users } = require('./state')
+const { users } = require('./state');
 
 /* BEGIN - create routes here */
+app.get('/hello', function (req, res) {
+  res.send('hi');
+});
+app.get('/hello/:name', function (req, res) {
+  let input = req.params.name;
+  res.send('hi ' + input);
+});
 
+app.post('/hi', function (req, res) {
+  let input = req.body;
+  console.log('body', input);
+  res.send('hi ' + input.name);
+});
 
+let list = [];
+
+app.get('/list', function (req, res) {
+  res.json(list);
+});
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
